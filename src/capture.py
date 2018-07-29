bc617a1fc7a78a2f6becb14191a49126850c12d2
Marcin Chalecki <alcaster0@gmail.com>
2018-07-29 11:21:14 +0200

    Working on multiprocessing.

--- src/capture.py
+++ src/capture.py
@@ -1,11 +1,10 @@
-import string
 import subprocess
 import time
 from threading import Thread
 from queue import Queue, Empty
 import io
 import logging
-from multiprocessing import Process, cpu_count, Queue as QueueMulti
+from multiprocessing import Process, Queue as QueueMulti
 
 from pydub import AudioSegment
 
@@ -22,62 +21,35 @@ class Capturer:
         self.cmd = cmd
         self.last_playing_update = None
         self.start_wave_nonblocking = None
-        self.sound_interval = .7
-        self.event_interval = 1
+        self.sound_interval = .6
+        self.event_interval = .3
 
         self.queue = QueueMulti()
 
-    def play_wave2(self):
-        consumer = Process(target=self.consume, args=(self.queue,))
-        consumer.start()
-        producer = Process(target=self.produce, args=(self.queue,))
+    def play_wave(self):
+        producer = Process(target=self.produce)
         producer.start()
+        consumer = Process(target=self.consume)
+        consumer.start()
 
-    def produce(self, queue):
-        idx = 0
-        start_time = time.time()
-        last_operation = time.time()
-        produce_interval = 0.2
-        while True:
-            if time.time() - last_operation > produce_interval:
-                print("putting")
-                queue.put(string.ascii_letters[idx])
-                idx+=1
-                last_operation = time.time()
-
-    def consume(self, queue):
-        start_time = time.time()
-        last_operation = time.time()
-        while True:
-            if time.time() - last_operation > self.event_interval:
-                current = []
-                while True:
-                    try:
-                        current.append(queue.get(False))
-                    except Empty:
-                        break
-                print(current)
-                last_operation = time.time()
-
-    def play_wave_nonblocking(self):  # todo collecting and adding to buffer should be in separate thread
-        first_skip = 1
-
+    def produce(self):
         ps = subprocess.Popen(self.cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         q = Queue()
         t = Thread(target=self.enqueue_output, args=(ps.stdout, q))
         t.daemon = True
         t.start()
 
-        buffer = bytearray()
+        last_playing_updated = time.time()
+        temporal_playing = False
         self.start_wave_nonblocking = time.time()
         not_play_start = self.start_wave_nonblocking
-        temporal_playing = False
+        last_operation = time.time()
+        buffer = bytearray()
 
-        current_value = 0
-        counter = 0
-        while time.time() - self.start_wave_nonblocking < 10:
+        playing = False
+        while True:
             try:
-                line = q.get_nowait()  # or q.get(timeout=.1)
+                line = q.get_nowait()
             except Empty:
                 if temporal_playing:
                     not_play_start = time.time()
@@ -86,55 +58,57 @@ class Capturer:
                 temporal_playing = True
                 buffer += line
 
-            if time.time() - self.start_wave_nonblocking < first_skip:  # skip first n seconds, sometimes first second is empty
-                continue
+            if time.time() - last_playing_updated > self.sound_interval:  # update self.playing
+                playing = self.get_playing(not_play_start, temporal_playing)
+                last_playing_updated = time.time()
 
-            if self.last_playing_update and time.time() - self.last_playing_update > self.sound_interval:  # update self.playing
-                self.set_is_playing(not_play_start, temporal_playing, first=False)
-
-            if time.time() - self.last_operation > self.event_interval:
-                if self.last_playing_update is None:  # if there is event and interval is shorter than update interval for self.playing
-                    self.set_is_playing(not_play_start, temporal_playing, first=True)
-                self.last_operation = time.time()
-
-                loudness_over_time = []
-                if buffer:
-                    iob = io.BytesIO(buffer)
-                    sound = AudioSegment.from_file(iob, format="raw", channels=1, sample_width=2, frame_rate=FRAMERATE)
-
-                    num_chunks = 1  # px
-                    chunk_size = int(len(sound) / num_chunks)
-                    if chunk_size > 0:
-                        for i in range(0, len(sound), chunk_size):
-                            chunk = sound[i:i + chunk_size]
-                            try:
-                                loudness_over_time.append(
-                                    chunk.rms)  # im not sure by what len(chunk) has to be divisible so there is double try
-                            except Exception:
-                                try:
-                                    loudness_over_time.append(chunk[:-1].rms)
-                                except Exception:
-                                    print("Cannot calculate rms.")
-                    if loudness_over_time:
-                        current_value = loudness_over_time[0]
-                        counter += 1
-
-                if not self.playing:
-                    current_value = 0
-
-                logger.info(current_value)
+            if time.time() - last_operation > self.event_interval:
+                last_operation = time.time()
+                self.queue.put((buffer, playing))
                 buffer = bytearray()
+
+    def consume(self):
+        start_time = time.time()
+        current_value = 0
+        counter = 0
+        while time.time() - start_time < 10:
+            loudness_over_time = []
+            current, is_playing = self.queue.get()
+            if current:
+                iob = io.BytesIO(current)
+                sound = AudioSegment.from_file(iob, format="raw", channels=1, sample_width=2, frame_rate=FRAMERATE)
+
+                num_chunks = 1  # px
+                chunk_size = int(len(sound) / num_chunks)
+                if chunk_size > 0:
+                    for i in range(0, len(sound), chunk_size):
+                        chunk = sound[i:i + chunk_size]
+                        try:
+                            loudness_over_time.append(
+                                chunk.rms)  # im not sure by what len(chunk) has to be divisible so there is double try
+                        except Exception:
+                            try:
+                                loudness_over_time.append(chunk[:-1].rms)
+                            except Exception:
+                                print("Cannot calculate rms.")
+                if loudness_over_time:
+                    current_value = loudness_over_time[0]
+                    counter += 1
+
+            if not is_playing:
+                current_value = 0
+
+            logger.info(current_value)
         logger.info(counter)
 
-    def set_is_playing(self, not_play_start, temporal_playing, first=False):  # handle first, if first is not playing
+    def get_playing(self, not_play_start, temporal_playing):
+        this_time = time.time()
         if temporal_playing:
-            self.playing = True
+            return True
+        elif this_time - self.start_wave_nonblocking < self.sound_interval:  # f.ex update every 1s this called after .5s
+            return False if not_play_start == self.start_wave_nonblocking else True
         else:
-            if first:
-                self.playing = False if not_play_start == self.start_wave_nonblocking else True
-            else:
-                self.playing = False if time.time() - not_play_start > self.sound_interval else True
-        self.last_playing_update = time.time()
+            return False if this_time - not_play_start > self.sound_interval else True
 
     @staticmethod
     def enqueue_output(out, queue):
@@ -146,7 +120,7 @@ class Capturer:
 def main():
     cmd = f"parec -d alsa_output.pci-0000_00_1b.0.analog-stereo.monitor --channels 1 --rate {FRAMERATE}"
     capturer = Capturer(cmd)
-    capturer.play_wave2()
+    capturer.play_wave()
 
 
 if __name__ == '__main__':
