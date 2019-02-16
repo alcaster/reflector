4d0f064088000949d4435adfee2322364b9fed3e
Marcin Chalecki <alcaster0@gmail.com>
2019-02-16 19:29:22 +0100

    Add sun mode to server

--- server/src/app.py
+++ server/src/app.py
@@ -2,11 +2,12 @@ import os
 
 import argparse
 import pigpio
+from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_restful import Api
 
-from endpoints import RGB, Audio
+from endpoints import RGB, Audio, Sun
 from pins import PINS
 from reflector import Reflector
 
@@ -18,6 +19,9 @@ parser.add_argument('--mute_factor', type=float, default=0.5,
 args = parser.parse_args()
 
 reflector = Reflector(100)
+scheduler = BackgroundScheduler()
+scheduler.start()
+
 pi = pigpio.pi()
 pins = PINS(pi)
 app = Flask(__name__)
@@ -25,6 +29,7 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(RGB, '/rgb', resource_class_kwargs={'pins': pins})
 api.add_resource(Audio, '/audio', resource_class_kwargs={'reflector': reflector, "args": args, "pins": pins})
+api.add_resource(Sun, '/sun', resource_class_kwargs={"pins": pins, "scheduler": scheduler})
 
 
 @app.route('/color')
@@ -39,3 +44,4 @@ if __name__ == '__main__':
         pi.set_PWM_dutycycle(PINS.RED, 0)
         pi.set_PWM_dutycycle(PINS.GREEN, 0)
         pi.set_PWM_dutycycle(PINS.BLUE, 0)
+        scheduler.shutdown()
--- server/src/endpoints.py
+++ server/src/endpoints.py
@@ -1,6 +1,9 @@
+import datetime
 import os
 
 from flask_restful import Resource, reqparse
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 class RGB(Resource):
     def __init__(self, pins):
@@ -41,3 +44,35 @@ class Audio(Resource):
         rgb = self.reflector.append(val)
         rgb = [int(self.args.mute_factor * i) for i in rgb]
         self.pins.set_value_to_all(*rgb)
+
+
+class Sun(Resource):
+    """
+    On execution iterates from (0,0,0) to (255,255,0)
+    """
+
+    def __init__(self, pins, scheduler):
+        self.pins = pins
+
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('time', type=int, help="Time in ms")
+        self.parser.add_argument('off', type=int, help="Time to turn off lights in ms")
+        self.parser.add_argument('token', type=str)
+        self.scheduler = scheduler
+        self.default_time_off = 600_000  # 10min
+
+    def post(self):
+        request_args = self.parser.parse_args()
+        if os.getenv("TOKEN") != request_args.token:
+            return 400
+        time = request_args.time
+        interval = time / 255
+        for i in range(255):
+            self.scheduler.add_job(func=self.pins.set_value_to_all, trigger="date",
+                                   run_date=datetime.datetime.now() + datetime.timedelta(milliseconds=i * interval),
+                                   args=(i, i, 0))
+
+        turn_off = request_args.off if request_args.off is not None else self.default_time_off
+        self.scheduler.add_job(func=self.pins.set_value_to_all, trigger="date",
+                               run_date=datetime.datetime.now() + datetime.timedelta(milliseconds=turn_off),
+                               args=(0, 0, 0))
